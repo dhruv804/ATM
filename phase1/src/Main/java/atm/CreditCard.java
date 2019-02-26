6684d517df4e62339668fa3a2fe993fe9229b2bb
Dhruv Patel <dhruvp68@gmail.com>
2019-02-25 22:03:37 -0500

    Started CreditCard

--- phase1/src/Main/java/atm/CreditCard.java
+++ phase1/src/Main/java/atm/CreditCard.java
@@ -1,4 +1,14 @@
 package atm;
 
-public class CreditCard {
+public class CreditCard extends Debt{
+
+    public CreditCard(User u) {
+        super(u);
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        balance += amount;
+        return true;
+    }
 }
