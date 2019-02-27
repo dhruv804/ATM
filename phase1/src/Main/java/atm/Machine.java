389b1618da01cf03455845236e3ccccc1bd28d34
vedantshah <vedant.shah@mail.utoronto.ca>
2019-02-27 14:48:11 -0500

    Started Machine class, made javadoc for Withdraw Method

--- phase1/src/Main/java/atm/Machine.java
+++ phase1/src/Main/java/atm/Machine.java
@@ -1,7 +1,56 @@
 package atm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by vedantshah on 2019-02-25.
  */
 public class Machine {
+    public Map<Integer, Integer> number_of_bills = new HashMap<Integer, Integer>();
+
+    public Machine() {
+        number_of_bills.put(5, 0);
+        number_of_bills.put(10, 0);
+        number_of_bills.put(20, 0);
+        number_of_bills.put(50, 0);
+        number_of_bills.put(100, 0);
+    }
+
+    public int get_number_of(int denom){
+        try {
+            return number_of_bills.get(denom);
+        } catch (NullPointerException n){
+            return -1;
+        }
+    }
+
+    public boolean insert_bill(int denom, int amount){
+        int prev = get_number_of(denom);
+        if (prev >= 0){
+            number_of_bills.put(denom, amount + prev);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * This method will subtract the bill denomination amounts, and figure out
+     * how to give give the cash to the consumer.
+     *
+     * @param cash: The amount to be withdrawn from the ATM
+     * @return true if can be done, false if not
+     */
+    public boolean withdraw(int cash){
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Machine m = new Machine();
+        m.insert_bill(21, 5);
+        m.insert_bill(20, 4);
+        System.out.println(m.get_number_of(21));
+
+    }
 }
