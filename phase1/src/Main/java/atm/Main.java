5b1803494213b84b1a83a453936d129828af5d76
vedantshah <vedant.shah@mail.utoronto.ca>
2019-03-04 14:07:28 -0500

    Bank Manager Update Machine

--- phase1/src/Main/java/atm/Main.java
+++ phase1/src/Main/java/atm/Main.java
@@ -1,7 +1,17 @@
 package atm;
 
+import javax.swing.*;
+
 /**
  * Created by vedantshah on 2019-03-04.
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+        BankManagerFrame b = new BankManagerFrame();
+        b.run();
+
+    }
+
 }
