14b5928561ffdc75203757e818f8c37fc541999c
Jaewon Uhr <jaewon.uhr@gmail.com>
2019-03-04 13:55:15 -0500

    added javadoc and additional functionality of withdraw method

--- phase1/src/Main/java/atm/Asset.java
+++ phase1/src/Main/java/atm/Asset.java
@@ -13,17 +13,38 @@ public abstract class Asset extends Account{
         this.balance = 0;
     }
 
+    /**
+     * This method removed a specified amount of money from the balance
+     * @param amount: the amount to withdraw from account
+     * @return true if successful, false if not
+     */
+
     @Override
     public boolean withdraw(double amount) {
-        balance -= amount;
-        return true;
+        if(balance >= amount){
+            balance -= amount;
+            return true;
+        }
+        return false;
     }
+
+
+    /**
+     * This method adds a specified amount of money to the balance
+     * @param amount: the amount to deposit to account
+     */
 
     @Override
     public void deposit(double amount) {
         balance += amount;
     }
 
+    /**
+     * This method transfers money from one account to another, if possible
+     * @param other_account Payee Account
+     * @param amount Amount to be transferred
+     * @return true if successful, false if not
+     */
 
     @Override
     public boolean transfer(Account other_account, double amount) {
