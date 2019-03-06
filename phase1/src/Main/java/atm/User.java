bacc1fa6c7cf0af72b265e72af7327170aa1d159
Jaewon Uhr <jaewon.uhr@gmail.com>
2019-03-06 17:27:59 -0500

    Added a list of requested accounts and a request_account method to User

--- phase1/src/Main/java/atm/User.java
+++ phase1/src/Main/java/atm/User.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class User {
     protected String name;
+    public ArrayList<Request> requested_accounts; //list of requested accounts from all users
     protected ArrayList<Account> account_list;
     protected String user_id;
     protected String user_pass;
@@ -12,6 +13,17 @@ public class User {
     public User(String name) {
         this.name = name;
         this.account_list = new ArrayList<>();
+    }
+
+    /**
+     * This method will allow users to request an account from the bank manager.
+     * It will add the request to a list of requests to be processed in the User class.
+     *
+     * @param account_type: The type of account requested for
+     */
+    public void request_account(Account account_type){
+        Request req = new Request(this,account_type);
+        requested_accounts.add(req);
     }
 
     public void set_user_id(String id){
