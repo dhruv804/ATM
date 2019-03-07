3df07be5d88d6f68c2c69c59f6fe0276a950f4d2
vedantshah <vedant.shah@mail.utoronto.ca>
2019-03-06 20:30:42 -0500

    Updates to tests

--- phase1/src/Test/java/atm/Manager_Test.java
+++ phase1/src/Test/java/atm/Manager_Test.java
@@ -14,33 +14,9 @@ public class Manager_Test {
     @BeforeEach
     public void setUp(){
         m1 = new BankManager();
-        u1 = new User("Ben");
+        u1 = new User("Ben", "ben", "pass");
         c1 = new Chequing();
         s1 = new Savings();
     }
 
-    @Test
-    public void request_test(){
-        m1.request_account(u1, c1);
-        m1.request_account(u1, s1);
-        assertTrue(m1.pending_requests.size() == 2);
-    }
-
-    @Test
-    public void add_account_test(){
-        m1.request_account(u1, c1);
-        m1.request_account(u1, s1);
-        m1.add_all_accounts();
-        assertTrue(u1.account_list.size() == 2);
-        assertTrue(m1.pending_requests.size() == 0);
-    }
-
-    @Test
-    public void get_info_test(){
-        m1.request_account(u1, c1);
-        m1.request_account(u1, s1);
-        for (BankManager.Request request: m1.pending_requests){
-            System.out.println(request.get_info());
-        }
-    }
 }
