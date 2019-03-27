774cf6544b1eed4abecd2c49bc49c0097b736c08
vedantshah <vedant.shah@mail.utoronto.ca>
2019-03-27 19:07:50 -0400

    Added Stock Portfolios

---
+++ phase1/src/Main/java/atm/StockPortfolio.java
@@ -0,0 +1,20 @@
+package atm;
+import java.util.Random;
+
+/**
+ * Created by vedantshah on 2019-03-27.
+ */
+public class StockPortfolio {
+    static double price = 5;
+    static double fee = 0.1;
+
+    public static void update_price(){
+        Random r = new Random();
+        double move_d = r.nextInt(3) - 0.814;
+        double move = (r.nextDouble() * move_d)/100.0;
+        //System.out.println(move);
+        price = (1 + move)*price;
+        price = Math.round(price*100) / 100.0;
+    }
+
+}
